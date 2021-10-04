35bd757f2916d46a0edd3a5425ba50943a8a4f62
oyuki-prog <oyu8no3jo@gmail.com>
2021-10-04 20:50:55 +0900

    アカウント登録画面に出身県の選択フォームを追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
@@ -12,8 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
+                            <div class="d-flex col-md-5">
+                                <label for="name" class="col-md-9 col-form-label text-md-right">{{ __('Name') }}</label>
+                                <div class="col-md-3 text-center required">必須</div>
+                            </div>
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
@@ -26,8 +28,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
+                            <div class="d-flex col-md-5">
+                                <label for="email" class="col-md-9 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                <div class="col-md-3 text-center required">必須</div>
+                            </div>
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
@@ -40,8 +44,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
+                            <div class="d-flex col-md-5">
+                                <label for="password" class="col-md-9 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <div class="col-md-3 text-center required">必須</div>
+                            </div>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
@@ -54,16 +60,38 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
+                            <div class="d-flex col-md-5">
+                                <label for="password-confirm" class="col-md-9 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                <div class="col-md-3 text-center required">必須</div>
+                            </div>
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <div class="d-flex col-md-5">
+                                <label for="prefecture_id" class="col-md-9 col-form-label text-md-right">{{ __('Prefecture of origin') }}</label>
+                            </div>
+                            <div class="col-md-6">
+                                <select name="prefecture_id" id="prefecture_id" class="form-control">
+                                        <option value="">---</option>
+                                    @foreach ($prefectures as $prefecture)
+                                        <option value="{{ $prefecture->id }}">{{ $prefecture->name }}</option>
+                                    @endforeach
+                                </select>
+
+                                @error('prefecture_id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-6 offset-md-5">
+                                <button type="submit" class="btn btn-primary px-5">
                                     {{ __('Register') }}
                                 </button>
                             </div>
